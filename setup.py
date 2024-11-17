3ae6754e0a3d52cf8a9025c07a8ac003b79e2acd
jpahm <20374744+jpahm@users.noreply.github.com>
2024-11-16 21:27:13 -0600

    Add extension to setup for compile args

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
-from setuptools import setup
+from setuptools import setup, Extension
 
 from Cython.Build import cythonize
 
+extensions = [
+    Extension(name="pyOpenDRIVE/*", sources=["pyOpenDRIVE/*.pyx"], include_dirs=["pyOpenDRIVE/include"], extra_compile_args=["-fpermissive"])
+]
+
 #setup(ext_modules=cythonize(["pyOpenDRIVE/*.pyx"]))
-setup(ext_modules=cythonize(["pyOpenDRIVE/*.pyx"]), include_dirs=["pyOpenDRIVE/include"])
+setup(ext_modules=cythonize(extensions))
