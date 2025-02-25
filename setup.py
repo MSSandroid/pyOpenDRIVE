32637a8bda10e6967d71698b94dc5148eedf5acc
MSSandroid <12539982+MSSandroid@users.noreply.github.com>
2025-02-25 16:09:59 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, Extension
 from Cython.Build import cythonize
 
 extensions = [
-    Extension(name="pyOpenDRIVE/*", sources=["pyOpenDRIVE/*.pyx"], include_dirs=["pyOpenDRIVE/include"], extra_compile_args=["-fpermissive"], compiler="mingw32")
+    Extension(name="pyOpenDRIVE/*", sources=["pyOpenDRIVE/*.pyx"], include_dirs=["pyOpenDRIVE/include"], extra_compile_args=["-fpermissive"])
 ]
 
 #setup(ext_modules=cythonize(["pyOpenDRIVE/*.pyx"]))
-setup(name="pyOpenDRIVE", version="0.1", ext_modules=cythonize(extensions))
+setup(name="pyOpenDRIVE", version="0.1", ext_modules=cythonize(extensions), compiler="mingw32")
